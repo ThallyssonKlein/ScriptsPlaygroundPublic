31bf6fc5c9dfd043b81ea55c4f58f43c75286967
Thallysson Klein <thallyssonklein@gmail.com>
2024-05-18 10:03:06 -0300

    feat: compare two csv

---
+++ compare_two_csv/main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+# Carrega os dois CSVs
+df1 = pd.read_csv('./csv_1.csv')
+
+import chardet    
+
+rawdata = open('./jira.csv', 'rb').read()
+result = chardet.detect(rawdata)
+encoding = result['encoding']
+df2 = pd.read_csv('./csv_2.csv', encoding=encoding)
+
+# Suponha que as colunas são 'X' em df1 e 'Y' em df2
+coluna_df1 = df1['Field']
+coluna_df2 = df2['Field']
+
+# Encontra os itens que estão apenas no primeiro CSV
+itens_somente_df1 = coluna_df1[~coluna_df1.isin(coluna_df2)].dropna()
+
+# Encontra os itens que estão apenas no segundo CSV
+itens_somente_df2 = coluna_df2[~coluna_df2.isin(coluna_df1)].dropna()
+
+print("Itens somente no primeiro CSV:")
+print(itens_somente_df1)
+
+print("Itens somente no segundo CSV:")
+print(itens_somente_df2)
