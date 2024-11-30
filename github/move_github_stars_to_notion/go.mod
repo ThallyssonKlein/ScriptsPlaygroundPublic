c7bacedb528326f67945cbc3962ba0dd682d6520
thallyssonklein <thallyssonklein@gmail.com>
2024-11-30 11:35:29 -0300

    feat: structure to send to queue move_github_stars_to_notion

---
+++ github/move_github_stars_to_notion/go.mod
@@ -0,0 +1,8 @@
+module move_github_stars_to_notion
+
+go 1.23.3
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/rabbitmq/amqp091-go v1.10.0
+)
--- github/move_github_stars_to_notion/main.go
+++ github/move_github_stars_to_notion/main.go
@@ -5,6 +5,10 @@ import (
     "fmt"
     "net/http"
     "os"
+    "log"
+
+    "github.com/joho/godotenv"
+    "github.com/rabbitmq/amqp091-go"
 )
 
 const githubAPI = "https://api.github.com/users/%s/starred?page=%d&per_page=100"
@@ -16,7 +20,60 @@ type Repository struct {
     HTMLURL     string `json:"html_url"`
 }
 
+func sendToRabbitMQ(queueName, message string) error {
+	// Conectar ao RabbitMQ
+	rabbitMQURL := os.Getenv("RABBITMQ_URL")
+	conn, err := amqp091.Dial(rabbitMQURL)
+	if err != nil {
+		return fmt.Errorf("falha ao conectar ao RabbitMQ: %w", err)
+	}
+	defer conn.Close()
+
+	// Criar um canal
+	channel, err := conn.Channel()
+	if err != nil {
+		return fmt.Errorf("falha ao criar canal: %w", err)
+	}
+	defer channel.Close()
+
+	// Declarar uma fila
+	_, err = channel.QueueDeclare(
+		queueName,
+		true,  // Durável
+		false, // Auto-delete
+		false, // Exclusiva
+		false, // Sem espera
+		nil,   // Argumentos adicionais
+	)
+	if err != nil {
+		return fmt.Errorf("falha ao declarar fila: %w", err)
+	}
+
+	// Publicar mensagem na fila
+	err = channel.PublishWithContext(
+		nil, // Contexto
+		"",  // Exchange
+		queueName, // Roteamento
+		false,     // Obrigatório
+		false,     // Imediato
+		amqp091.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("falha ao publicar mensagem: %w", err)
+	}
+
+	return nil
+}
+
 func main() {
+    err2 := godotenv.Load()
+	if err2 != nil {
+		log.Fatalf("Erro ao carregar o arquivo .env: %v", err)
+	}
+
     if len(os.Args) < 2 {
         fmt.Println("Usage: go run main.go <github-username>")
         return
@@ -52,6 +109,10 @@ func main() {
 
         for _, repo := range repos {
             fmt.Printf("Name: %s\nFull Name: %s\nDescription: %s\nURL: %s\n\n", repo.Name, repo.FullName, repo.Description, repo.HTMLURL)
+            err := sendToRabbitMQ("repo_urls", repo.HTMLURL)
+			if err != nil {
+				log.Printf("Erro ao enviar mensagem ao RabbitMQ: %v", err)
+			}
         }
 
         page++
